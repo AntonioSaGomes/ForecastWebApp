c403646b952aeb1f68d2d364158b817ed5d0c202
SaGomes <antonio97@ua.pt>
2019-05-06 13:36:20 +0100

    add some tests

--- tqs_project_80124/src/test/java/com/ua/tqs_project_80124/API_Test.java
+++ tqs_project_80124/src/test/java/com/ua/tqs_project_80124/API_Test.java
@@ -5,10 +5,45 @@
  */
 package com.ua.tqs_project_80124;
 
+import java.net.URL;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  *
  * @author sagomes
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class API_Test {
     
+    @LocalServerPort
+    private int port;
+    
+    private URL base;
+    
+    @Autowired
+    private TestRestTemplate template;
+    
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/");
+       
+    }
+    
+    @Test
+    public void getWeather() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(),equalTo("Hello World"));
+                
+    }
 }
--- tqs_project_80124/src/test/java/com/ua/tqs_project_80124/WebInterfaceTest.java
+++ tqs_project_80124/src/test/java/com/ua/tqs_project_80124/WebInterfaceTest.java
@@ -5,10 +5,52 @@
  */
 package com.ua.tqs_project_80124;
 
+import java.net.URL;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  *
  * @author sagomes
  */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WebInterfaceTest {
+    
+    @Autowired
+    private MockMvc mvc;
+    
+    @Test
+    public void getWeather() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.ALL)).andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string(equalTo("Hello")));
+        
+    }
+}
+    
+    
+    
+    
+    
+    
+    
     
 }
